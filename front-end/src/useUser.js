730b74686452a500b44fd3e9a31431399c8ffb0f
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-14 19:12:41 -0700

    Created useUser hook

--- front-end/src/useUser.js
+++ front-end/src/useUser.js
@@ -1,1 +1,28 @@
+import { useState,useEffect } from "react";
+import { useToken } from "./useToken";
 
+export const useUser=()=>{
+  const[token]=useToken();
+  const getPayloafFromToken=token=>{
+    const encodedPayLoad=token.split('.')[1];
+    return JSON.parse(atob(encodedPayLoad));
+    
+  }
+   const[user,setUser]=useState(()=>{
+    if(!token){
+      return null;
+    }
+    else{
+      return getPayloafFromToken(token)
+    }
+   })
+   useEffect(()=>{
+      if(!token){
+        setUser(null);
+      }
+      else{
+        setUser(getPayloafFromToken(token))
+      }
+   },[token])
+   return user;
+}
